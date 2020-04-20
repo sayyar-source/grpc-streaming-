07613150ed18bbfefc48f5a2dd42a73fc84e49bf
eng.sayyar <eng.sayyar@gmail.com>
2020-04-21 01:09:14 +0300

    server to slient

--- StreamClient/Program.cs
+++ StreamClient/Program.cs
@@ -1,6 +1,8 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using GrpcServiceStream;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using static GrpcServiceStream.Numerics;
 
@@ -15,32 +17,34 @@ namespace StreamClient
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
 
             var client = new GrpcServiceStream.Numerics.NumericsClient(channel);
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(20));
+
+            var request = new NumberResponse() { Result = 20 };
+            using var streamingCall = client.SendNumber(request, cancellationToken: cts.Token);
+
+            try
+            {
+                await foreach (var number in streamingCall.ResponseStream.ReadAllAsync(cancellationToken: cts.Token))
+                {
+
+                    Console.WriteLine($"{number.Value}");
+                }
 
 
-            await StreamNumbersFromClientToServer(client);
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                Console.WriteLine("Stream cancelled.");
+            }
+
+
+
 
             Console.ReadLine();
 
         }
-        static Random RNG = new Random();
+      
 
-        private static async Task StreamNumbersFromClientToServer(NumericsClient client)
-        {
-            using (var call = client.SendNumber())
-            {
-                for (var i = 0; i < 10; i++)
-                {
-                    var number = RNG.Next(5);
-                    Console.WriteLine($"Sending {number}");
-                    await call.RequestStream.WriteAsync(new NumberRequest { Value = number });
-                    await Task.Delay(1000);
-                }
-
-                await call.RequestStream.CompleteAsync();
-
-                var response = await call;
-                Console.WriteLine($"Result: {response.Result}");
-            }
-        }
+     
     }
 }
